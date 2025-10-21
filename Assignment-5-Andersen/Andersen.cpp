ef21214a77eef8940ddf80c3634bb229402239e6
chenlu20 <chen181396@163.com>
2025-10-21 13:05:24 +0800

    Update Andersen.cpp

--- Assignment-5-Andersen/Andersen.cpp
+++ Assignment-5-Andersen/Andersen.cpp
@@ -1,3 +1,43 @@
+// /**
+//  * Andersen.cpp
+//  * @author kisslune
+//  */
+
+// #include "A5Header.h"
+
+// using namespace llvm;
+// using namespace std;
+
+// int main(int argc, char** argv)
+// {
+//     auto moduleNameVec =
+//             OptionBase::parseOptions(argc, argv, "Whole Program Points-to Analysis",
+//                                      "[options] <input-bitcode...>");
+
+//     SVF::LLVMModuleSet::buildSVFModule(moduleNameVec);
+
+//     SVF::SVFIRBuilder builder;
+//     auto pag = builder.build();
+//     auto consg = new SVF::ConstraintGraph(pag);
+//     consg->dump();
+
+//     Andersen andersen(consg);
+
+//     // TODO: complete the following method
+//     andersen.runPointerAnalysis();
+
+//     andersen.dumpResult();
+//     SVF::LLVMModuleSet::releaseLLVMModuleSet();
+// 	return 0;
+// }
+
+
+// void Andersen::runPointerAnalysis()
+// {
+//     // TODO: complete this method. Point-to set and worklist are defined in A5Header.h
+//     //  The implementation of constraint graph is provided in the SVF library
+// }
+
 /**
  * Andersen.cpp
  * @author 3220252746
@@ -30,6 +70,7 @@ int main(int argc, char** argv)
     SVF::LLVMModuleSet::releaseLLVMModuleSet();
 	return 0;
 }
+
 
 void Andersen::runPointerAnalysis()
 {
@@ -140,4 +181,3 @@ void Andersen::runPointerAnalysis()
         }
     }
 }
-
