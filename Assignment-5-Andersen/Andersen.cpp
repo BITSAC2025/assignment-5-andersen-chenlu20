6ef4df7fe54187b24f4a46c7ce20eecc4f0b279a
chenlu20 <chen181396@163.com>
2025-11-20 18:55:20 +0800

    Update Andersen.cpp

--- Assignment-5-Andersen/Andersen.cpp
+++ Assignment-5-Andersen/Andersen.cpp
@@ -29,63 +29,82 @@ void Andersen::runPointerAnalysis()
 {
     WorkList<SVF::NodeID> worklist;
     
-    // 初始化
+    // Step 1: Address 规则 (o --Addr--> p)
     for (auto iter = consg->begin(); iter != consg->end(); ++iter) {
         SVF::ConstraintNode* node = iter->second;
-        for (auto edge : node->getAddrInEdges()) {
-            if (pts[edge->getDstID()].insert(edge->getSrcID()).second) {
-                worklist.push(edge->getDstID());
+        SVF::NodeID objId = node->getId();  // 当前节点作为对象
+        
+        // 遍历从对象出发的 Address 出边
+        for (auto edge : node->getAddrOutEdges()) {
+            SVF::NodeID ptrId = edge->getDstID();
+            if (pts[ptrId].insert(objId).second) {
+                worklist.push(ptrId);
             }
         }
     }
     
-    // 主循环
+    // Step 2: 主循环
     while (!worklist.empty()) {
-        SVF::NodeID n = worklist.pop();
-        SVF::ConstraintNode* node = consg->getConstraintNode(n);
+        SVF::NodeID p = worklist.pop();
+        SVF::ConstraintNode* nodeP = consg->getConstraintNode(p);
         
-        // Copy
-        for (auto e : node->getCopyOutEdges()) {
-            size_t old = pts[e->getDstID()].size();
-            pts[e->getDstID()].insert(pts[n].begin(), pts[n].end());
-            if (pts[e->getDstID()].size() > old) {
-                worklist.push(e->getDstID());
+        // Store 规则: *p = q (需要动态添加 q->o 的 Copy 边)
+        for (auto edge : nodeP->getStoreInEdges()) {
+            SVF::NodeID q = edge->getSrcID();
+            
+            for (SVF::NodeID o : pts[p]) {
+                SVF::ConstraintNode* qNode = consg->getConstraintNode(q);
+                SVF::ConstraintNode* oNode = consg->getConstraintNode(o);
+                
+                if (!consg->hasEdge(qNode, oNode, SVF::ConstraintEdge::Copy)) {
+                    consg->addCopyCGEdge(q, o);
+                    worklist.push(q);
+                }
             }
         }
         
-        // Load
-        for (auto e : node->getLoadOutEdges()) {
-            size_t old = pts[e->getDstID()].size();
-            for (auto o : pts[n]) {
-                pts[e->getDstID()].insert(pts[o].begin(), pts[o].end());
-            }
-            if (pts[e->getDstID()].size() > old) {
-                worklist.push(e->getDstID());
-            }
-        }
-        
-        // Store
-        for (auto e : node->getStoreOutEdges()) {
-            for (auto o : pts[n]) {
-                size_t old = pts[o].size();
-                pts[o].insert(pts[e->getSrcID()].begin(), pts[e->getSrcID()].end());
-                if (pts[o].size() > old) {
+        // Load 规则: r = *p (需要动态添加 o->r 的 Copy 边)
+        for (auto edge : nodeP->getLoadOutEdges()) {
+            SVF::NodeID r = edge->getDstID();
+            
+            for (SVF::NodeID o : pts[p]) {
+                SVF::ConstraintNode* oNode = consg->getConstraintNode(o);
+                SVF::ConstraintNode* rNode = consg->getConstraintNode(r);
+                
+                if (!consg->hasEdge(oNode, rNode, SVF::ConstraintEdge::Copy)) {
+                    consg->addCopyCGEdge(o, r);
                     worklist.push(o);
                 }
             }
         }
         
-        // Gep - 显式指定 APOffset 类型
-        for (auto e : node->getGepOutEdges()) {
-            bool changed = false;
-            for (auto o : pts[n]) {
-                // 使用 SVF::APOffset(0) 显式指定类型
-                if (pts[e->getDstID()].insert(consg->getGepObjVar(o, SVF::APOffset(0))).second) {
-                    changed = true;
-                }
+        // Copy 规则: p = q
+        for (auto edge : nodeP->getCopyOutEdges()) {
+            SVF::NodeID x = edge->getDstID();
+            size_t oldSize = pts[x].size();
+            pts[x].insert(pts[p].begin(), pts[p].end());
+            if (pts[x].size() > oldSize) {
+                worklist.push(x);
             }
-            if (changed) {
-                worklist.push(e->getDstID());
+        }
+        
+        // Gep 规则: x = &p->field
+        for (auto edge : nodeP->getGepOutEdges()) {
+            // 转换为 NormalGepCGEdge 获取常量偏移
+            SVF::NormalGepCGEdge* gepEdge = SVF::SVFUtil::dyn_cast<SVF::NormalGepCGEdge>(edge);
+            if (!gepEdge) continue;
+            
+            SVF::NodeID x = gepEdge->getDstID();
+            SVF::APOffset offset = gepEdge->getConstantFieldIdx();
+            
+            size_t oldSize = pts[x].size();
+            for (SVF::NodeID o : pts[p]) {
+                SVF::NodeID fieldObj = consg->getGepObjVar(o, offset);
+                pts[x].insert(fieldObj);
+            }
+            
+            if (pts[x].size() > oldSize) {
+                worklist.push(x);
             }
         }
     }
